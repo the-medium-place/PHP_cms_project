073ac18206163ed86f87cf0c23a45a4ee8fe5d43
ZGS <zgstowell@gmail.com>
2020-07-27 11:56:42 -0700

    admin/update posts fully functioning

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,14 +1,10 @@
 <?php
 
-if(isset($_GET['p_id'])){
+if (isset($_GET['p_id'])) {
     $post_id = $_GET['p_id'];
-
-
-
-
 }
 
-$query = "SELECT * FROM posts";
+$query = "SELECT * FROM posts WHERE post_id = $post_id";
 $select_posts_by_id = mysqli_query($connection, $query);
 
 // CREATE CATEGORY TABLE LISTINGS
@@ -25,6 +21,48 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_content = $row['post_content'];
 }
 
+// CAPTURE UPDATE VALUES
+if (isset($_POST['update_post'])) {
+
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['post_image']['name'];
+    $post_image_temp = $_FILES['post_image']['tmp_name'];
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    // CHECK FOR VALUE IN IMAGE SPACE
+    // BEFORE SENDING UPDATE QUERY
+    if(empty($post_image)) {
+
+        $query = "SELECT * FROM posts WHERE post_id = $post_id";
+        $select_image = mysqli_query($connection, $query);
+
+        while($row = mysqli_fetch_array($select_image)){
+            $post_image = $row['post_image'];
+        }
+    }
+
+    // BUILD UPDATE QUERY
+    $query = "UPDATE posts SET ";
+    $query .= "post_title = '" . $post_title . "', ";
+    $query .= "post_category_id = '" . $post_category_id . "', ";
+    $query .= "post_date = now(), ";
+    $query .= "post_author = '" . $post_author . "', ";
+    $query .= "post_status = '" . $post_status . "', ";
+    $query .= "post_tags = '" . $post_tags . "', ";
+    $query .= "post_content = '" . $post_content . "', ";
+    $query .= "post_image = '" . $post_image . "' ";
+    $query .= "WHERE post_id = " . $post_id . ";";
+
+    $update_post = mysqli_query($connection, $query);
+    confirm($update_post);
+
+}
 
 ?>
 
@@ -33,13 +71,31 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 <form action="" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
-        <label for="title">Post Title</label>
-        <input value="<?php echo $post_title ?>" type="text" name="title" class="form-control">
+        <label for="post_title">Post Title</label>
+        <input value="<?php echo $post_title ?>" type="text" name="post_title" class="form-control">
     </div>
 
     <div class="form-group">
-        <label for="post_category_id">Post Category</label>
-        <input value="<?php echo $post_category_id ?>" type="text" name="post_category_id" class="form-control">
+        <label for="post_category">Select Category </label>
+        <br>
+        <select name ="post_category" id="">
+        <?php
+$query = "SELECT * FROM categories";
+$update_query = mysqli_query($connection, $query);
+
+confirm($update_query);
+
+while ($row = mysqli_fetch_assoc($update_query)) {
+
+    $cat_id = $row['id'];
+    $cat_title = $row['cat_title'];
+    echo "<option value='" . $cat_id . "'>" . $cat_title . "</option>";
+
+}
+?>
+          </select>
+
+
     </div>
 
     <div class="form-group">
@@ -53,8 +109,12 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
 
     <div class="form-group">
-        <label for="post_image">Post Image</label>
+    <!-- <label for="post_img">Image </label> -->
+    <!-- <br> -->
+        <label for="post_image">Post Image</label><br>
+        <img src="../images/<?php echo $post_image ?>" width="100" name="post_image"/>
         <input type="file" name="post_image" class="form-control">
+
     </div>
 
     <div class="form-group">
@@ -64,12 +124,10 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea name="post_content" id="" cols="30" rows="10" class="form-control">
-        <?php echo $post_content ?> 
-        </textarea>
+        <textarea name="post_content" id="" cols="30" rows="10" class="form-control"><?php echo $post_content ?></textarea>
     </div>
 
     <div class="form-grop">
-        <input type="submit" value="Publish" class="btn btn-primary" name="create_post">
+        <input type="submit" value="Publish" class="btn btn-primary" name="update_post">
     </div>
 </form>
