c8869abc2e06415697ac947d29e71dc7b9b04e01
ZGS <zgstowell@gmail.com>
2020-08-30 00:10:33 -0700

    figured out a login validation method - will it match the next videos?

--- includes/login.php
+++ includes/login.php
@@ -6,11 +6,20 @@
     $username = $_POST['username'];
     $user_password = $_POST['user_password'];
 
-    
+    $query = "SELECT * FROM users WHERE username = '$username' AND user_password = '$user_password'";
 
+    $user_query = mysqli_query($connection, $query);
 
+    $loggedIn = false;
 
+    while ($row = mysqli_fetch_assoc($user_query)){
+        $loggedIn = true;
+        echo 'logged in';
+    }
 
+    if(!$loggedIn){
+        echo 'not logged in';
+    }
 
 ?>
 
